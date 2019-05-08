ada040ec9c9c3efeb37b8e7182285895c534ab8d
arushka <kuatbaeva.aruzhan@mail.ru>
2019-05-09 00:05:52 +0600

    last

---
+++ week14/todo-back/myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\kuatb\desktop\webdev2019\week14\todo-back\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
